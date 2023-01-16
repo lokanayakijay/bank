9ca951a8bd43a0743c5bd6d62c058a34a4f73c02
lokanayakijay <122774314+lokanayakijay@users.noreply.github.com>
2023-01-16 12:43:53 +0530

    Add files via upload

---
+++ src/main/java/org/example/AccountHolder.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class AccountHolder {
+    public static void main(String[] args) {
+        Bank b1 = new Bank();
+        b1.setminimumBalance();
+        b1.getminimumBalance();
+
+
+
+    }
+}
---
+++ src/main/java/org/example/Bank.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Bank {
+    int minimumBalance=2000;
+
+
+    public int getminimumBalance() {
+        return this.minimumBalance;
+
+    }
+    public int setminimumBalance(){
+        return this.minimumBalance;
+    }
+
+}
